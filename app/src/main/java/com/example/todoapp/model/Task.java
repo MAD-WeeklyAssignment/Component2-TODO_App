908ea3dcbc4a634ccb26eb592a5cc83a2cfa5e15
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-03-31 20:22:35 +0545

    Create a Task constructor

--- app/src/main/java/com/example/todoapp/model/Task.java
+++ app/src/main/java/com/example/todoapp/model/Task.java
@@ -28,6 +28,13 @@ public class Task {
     @ColumnInfo(name = "is_done")
     public boolean isDone;
 
+    public Task(String task, Priority priority, Date dueDate, Date dateCreated, boolean isDone) {
+        this.task = task;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.dateCreated = dateCreated;
+        this.isDone = isDone;
+    }
 
 
 }
