b395ad37f49eb7cad29b64bab8b73abdb1d28bed
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-03-31 20:26:00 +0545

    add toString method

--- app/src/main/java/com/example/todoapp/model/Task.java
+++ app/src/main/java/com/example/todoapp/model/Task.java
@@ -1,17 +1,15 @@
 package com.example.todoapp.model;
 
-import android.renderscript.RenderScript;
-
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
 
-@Entity(tableName="task_table")
+@Entity(tableName = "task_table")
 public class Task {
 
-    @ColumnInfo(name="task_id")
+    @ColumnInfo(name = "task_id")
     @PrimaryKey(autoGenerate = true)
     public long taskId;
 
@@ -19,7 +17,7 @@ public class Task {
 
     public Priority priority;
 
-    @ColumnInfo(name="due_date")
+    @ColumnInfo(name = "due_date")
     public Date dueDate;
 
     @ColumnInfo(name = "created_at")
@@ -82,5 +80,17 @@ public class Task {
 
     public void setDone(boolean done) {
         isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskId=" + taskId +
+                ", task='" + task + '\'' +
+                ", priority=" + priority +
+                ", dueDate=" + dueDate +
+                ", dateCreated=" + dateCreated +
+                ", isDone=" + isDone +
+                '}';
     }
 }
