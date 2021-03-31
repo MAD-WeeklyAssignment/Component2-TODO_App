5ab1aecb564afc04179dee32379ea2916c985869
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-03-31 21:11:23 +0545

    declare deleteAll method

--- app/src/main/java/com/example/todoapp/data/TaskDao.java
+++ app/src/main/java/com/example/todoapp/data/TaskDao.java
@@ -2,12 +2,18 @@ package com.example.todoapp.data;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.todoapp.model.Task;
 
 @Dao
 public interface TaskDao {
 
     @Insert
     void insertTask(Task task);
+
+    @Query("Delete FROM task_table")
+    void deleteAll();
 
 
 }
