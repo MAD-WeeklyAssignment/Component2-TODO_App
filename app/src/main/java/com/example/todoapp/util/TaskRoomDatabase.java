143aa8f100eed7213b65b36c05c2c1ff3c00e680
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-03-31 20:53:00 +0545

    Creating singlton TaskRoomDatabase

--- app/src/main/java/com/example/todoapp/util/TaskRoomDatabase.java
+++ app/src/main/java/com/example/todoapp/util/TaskRoomDatabase.java
@@ -1,8 +1,13 @@
 package com.example.todoapp.util;
 
-import androidx.annotation.NonNull;
-import androidx.room.RoomDatabase;
+import android.content.Context;
 
+import androidx.annotation.NonNull;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.security.PublicKey;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,7 +15,34 @@ public abstract class TaskRoomDatabase extends RoomDatabase {
 
     public static final int NUMBER_OF_THREADS = 4;
     public static volatile TaskRoomDatabase INSTANCE;
+    public static final String DATASE_NAME = "todo_database";
     public static final ExecutorService databaseWriterExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static final RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            databaseWriterExecutor.execute(()->{
+
+            });
+        }
+    }
+    public static TaskRoomDatabase getDatabase(final Context context){
+        if(INSTANCE==null){
+            synchronized (TaskRoomDatabase.class){
+                if (INSTANCE==null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskRoomDatabase.class, DATASE_NAME)
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+
+                }
+            }
+        }
+        return INSTANCE;
+
+    }
+
 
 
 }
