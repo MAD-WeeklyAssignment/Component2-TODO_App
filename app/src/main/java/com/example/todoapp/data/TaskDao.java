a6e1258c57aaccb3f507a289234602fe3bd8e4d8
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-03-31 21:08:40 +0545

    declare method to insert task

--- app/src/main/java/com/example/todoapp/data/TaskDao.java
+++ app/src/main/java/com/example/todoapp/data/TaskDao.java
@@ -1,4 +1,13 @@
 package com.example.todoapp.data;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+@Dao
 public interface TaskDao {
+
+    @Insert
+    void insertTask(Task task);
+
+
 }
