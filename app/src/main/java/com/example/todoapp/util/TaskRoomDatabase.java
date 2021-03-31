498623bfd93ae6c874324831a7661f24b9039e27
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-03-31 20:36:37 +0545

    declear threads and instance

--- app/src/main/java/com/example/todoapp/util/TaskRoomDatabase.java
+++ app/src/main/java/com/example/todoapp/util/TaskRoomDatabase.java
@@ -1,6 +1,16 @@
 package com.example.todoapp.util;
 
+import androidx.annotation.NonNull;
 import androidx.room.RoomDatabase;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public static final int NUMBER_OF_THREADS = 4;
+    public static volatile TaskRoomDatabase INSTANCE;
+    public static final ExecutorService databaseWriterExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+
 }
