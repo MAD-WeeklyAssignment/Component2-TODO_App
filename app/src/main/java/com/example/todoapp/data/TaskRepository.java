6ce1a0434afc96045189f425b4ceb4a189c08e9f
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 06:55:26 +0545

    Create an instance of database

--- app/src/main/java/com/example/todoapp/data/TaskRepository.java
+++ app/src/main/java/com/example/todoapp/data/TaskRepository.java
@@ -6,13 +6,17 @@ import android.app.Application;
 import androidx.lifecycle.LiveData;
 
 import com.example.todoapp.model.Task;
+import com.example.todoapp.util.TaskRoomDatabase;
 
 import java.util.List;
 
 public class TaskRepository {
 
-    public final TaskDao;
+    private final TaskDao taskDao;
     private final LiveData<List<Task>> allTasks;
 
+    public TaskRepository(Application application) {
+        TaskRoomDatabase database = TaskRoomDatabase.getDatabase(application);
 
+    }
 }
