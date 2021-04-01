35cefbb1b3a272e3fb5988f3b737d711a3765a5c
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 08:31:07 +0545

    Added Date method to convert received value into Date formate

--- app/src/main/java/com/example/todoapp/util/Converter.java
+++ app/src/main/java/com/example/todoapp/util/Converter.java
@@ -1,4 +1,10 @@
 package com.example.todoapp.util;
 
+import java.util.Date;
+
 public class Converter {
+
+    public static Date fromTimeStamp(Long value){
+        return value == null ? null : new Date(value);
+    }
 }
