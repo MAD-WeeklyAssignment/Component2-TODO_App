7bb1bc79fd82dce631f916c7d9145739a840a091
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 08:35:11 +0545

    method to convert date to time stamp

--- app/src/main/java/com/example/todoapp/util/Converter.java
+++ app/src/main/java/com/example/todoapp/util/Converter.java
@@ -10,4 +10,9 @@ public class Converter {
     public static Date fromTimeStamp(Long value){
         return value == null ? null : new Date(value);
     }
+
+    @TypeConverter
+    public static Long dateToTimeStamp(Date date){
+        return value == null ? null : date.getTime();
+    }
 }
