5afe920a940e749c3ae18d02c715b79e3683b0fa
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 08:32:26 +0545

    add annotation for Date method

--- app/src/main/java/com/example/todoapp/util/Converter.java
+++ app/src/main/java/com/example/todoapp/util/Converter.java
@@ -1,9 +1,12 @@
 package com.example.todoapp.util;
 
+import androidx.room.TypeConverter;
+
 import java.util.Date;
 
 public class Converter {
 
+    @TypeConverter
     public static Date fromTimeStamp(Long value){
         return value == null ? null : new Date(value);
     }
