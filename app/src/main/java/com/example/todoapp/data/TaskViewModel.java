ef1dec6d700d446dfb9f43a60cee77e9c68cfee0
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 07:30:24 +0545

    Instantiate repository

--- app/src/main/java/com/example/todoapp/data/TaskViewModel.java
+++ app/src/main/java/com/example/todoapp/data/TaskViewModel.java
@@ -17,5 +17,6 @@ public class TaskViewModel extends AndroidViewModel {
 
     public TaskViewModel(@NonNull Application application) {
         super(application);
+        repository = new TaskRepository(application);
     }
 }
