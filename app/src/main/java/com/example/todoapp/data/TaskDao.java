e0c0d04c2690af46156f26b082a787eff219a465
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 06:25:24 +0545

    add delete method

--- app/src/main/java/com/example/todoapp/data/TaskDao.java
+++ app/src/main/java/com/example/todoapp/data/TaskDao.java
@@ -2,6 +2,7 @@ package com.example.todoapp.data;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
@@ -27,6 +28,9 @@ public interface TaskDao {
 
     @Update
     void update(Task task);
+
+    @Delete
+    void delete(Task task);
 
 
 }
