c48c4a7e52b7e1281cc6d8b137fb9b2411ac9281
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 21:42:30 +0545

    Get all view from todo_row layout

--- app/src/main/java/com/example/todoapp/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/todoapp/adapter/RecyclerViewAdapter.java
@@ -1,12 +1,18 @@
 package com.example.todoapp.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatRadioButton;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todoapp.R;
 import com.example.todoapp.model.Task;
+import com.google.android.material.chip.Chip;
 
 import java.util.List;
 
@@ -21,7 +27,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.todo_row,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -35,8 +43,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        public AppCompatRadioButton radioButton;
+        public AppCompatTextView task;
+        public Chip todayChip;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            radioButton = itemView.findViewById(R.id.todo_radio_button);
+            todayChip = itemView.findViewById(R.id.today_chip);
+            task = itemView.findViewById(R.id.todo_row_todo);
         }
     }
 }
