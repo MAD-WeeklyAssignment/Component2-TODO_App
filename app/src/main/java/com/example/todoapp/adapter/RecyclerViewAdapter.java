bfbb69489a454b8939a198579d517ad20da3df98
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 21:47:07 +0545

    Get size of task list

--- app/src/main/java/com/example/todoapp/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/todoapp/adapter/RecyclerViewAdapter.java
@@ -40,7 +40,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return taskList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
