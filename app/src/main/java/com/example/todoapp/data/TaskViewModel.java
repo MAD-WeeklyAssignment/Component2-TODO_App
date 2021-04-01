9c610635ade3270eda905adfaa8bae6b93bbe344
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 07:24:37 +0545

    create constructor for TaskViewModel class

--- app/src/main/java/com/example/todoapp/data/TaskViewModel.java
+++ app/src/main/java/com/example/todoapp/data/TaskViewModel.java
@@ -1,6 +1,13 @@
 package com.example.todoapp.data;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
 public class TaskViewModel extends AndroidViewModel {
+
+    public TaskViewModel(@NonNull Application application) {
+        super(application);
+    }
 }
