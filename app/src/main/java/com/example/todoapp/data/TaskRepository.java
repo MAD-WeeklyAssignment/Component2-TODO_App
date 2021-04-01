a944a1729e0ddf6617926fc0c981d2c740d960e9
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 06:50:22 +0545

    declare LiveData that contain all task list

--- app/src/main/java/com/example/todoapp/data/TaskRepository.java
+++ app/src/main/java/com/example/todoapp/data/TaskRepository.java
@@ -1,5 +1,18 @@
 package com.example.todoapp.data;
 
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.todoapp.model.Task;
+
+import java.util.List;
+
 public class TaskRepository {
+
+    public final TaskDao;
+    private final LiveData<List<Task>> allTasks;
+
 
 }
