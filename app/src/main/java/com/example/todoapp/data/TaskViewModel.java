f34f669725649fbb824637fa94fd39f675d0a755
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-01 07:28:30 +0545

    Declare field

--- app/src/main/java/com/example/todoapp/data/TaskViewModel.java
+++ app/src/main/java/com/example/todoapp/data/TaskViewModel.java
@@ -4,8 +4,16 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.todoapp.model.Task;
+
+import java.util.List;
 
 public class TaskViewModel extends AndroidViewModel {
+
+    public static TaskRepository repository;
+    public final LiveData<List<Task>> allTasks;
 
     public TaskViewModel(@NonNull Application application) {
         super(application);
