901ab73aaab23f5a0703fb05ef90443129f0e329
Aditya8014 <48464775+Aditya8014@users.noreply.github.com>
2021-04-02 13:58:34 +0545

    set method to edit task

--- app/src/main/java/com/example/todoapp/model/SharedViewModel.java
+++ app/src/main/java/com/example/todoapp/model/SharedViewModel.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel;
 public class SharedViewModel extends ViewModel {
 
     private final MutableLiveData<Task> selectedItem = new MutableLiveData<>();
+    private boolean isEdit;
 
     public void selectItem(Task task){
         selectedItem.setValue(task);
@@ -14,5 +15,13 @@ public class SharedViewModel extends ViewModel {
 
     public LiveData<Task> getSelectedItem(){
         return selectedItem;
+    }
+
+    public void setIsEdit(boolean isEdit){
+        this.isEdit = isEdit;
+    }
+
+    public boolean getIsEdit(){
+        return isEdit;
     }
 }
